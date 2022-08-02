f1684a4e252af9a2bd912e6386aa7b623e35a306
AngelAzrail <androsovdima99@gmail.com>
2022-08-02 17:41:58 +0300

    view switcher init

---
+++ src/ui/Controls/ViewSwitcher.tsx
@@ -0,0 +1,4 @@
+const ViewSwitcher = () => {
+  return <div></div>;
+};
+export default ViewSwitcher;
--- src/ui/Map/Layers/Source/Source.tsx
+++ src/ui/Map/Layers/Source/Source.tsx
@@ -10,7 +10,6 @@ import HeatMap from '../Vector/HeatMap';
 import ClusterLayer from '../Vector/Cluster';
 
 const Source = (): JSX.Element => {
-
   const source = useMemo(() => new VectorSource(), []);
 
   useEffect(() => {
@@ -18,7 +17,7 @@ const Source = (): JSX.Element => {
       .get('https://metrics.dtechs.io/dem-api/analytics/geo?from=2022-01-01&to=2022-02-01', {
         headers: {
           authorization:
-            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY1OTQ0MDg5NCwiaWF0IjoxNjU5NDMxODk0fQ.qmAeEXZe3jK088QYL1-9VifDOh8WO5mlreG6VFivMOgb_xZpFXBUGkh5JV6_Q81SbHjQXHJ1x641vHl1Dr-vyA',
+            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY1OTQ1ODg1MSwiaWF0IjoxNjU5NDQ5ODUxfQ.kM51VlQ_uAoTgv-nGrQJqZ7pKO-5GWL4Ceo6kymzC2wFYVZg9PrJ5W6O4IRyj2FKa3psfsw4kHTO0HApzFzc2A',
         },
       })
       .then(({ data }) => {
