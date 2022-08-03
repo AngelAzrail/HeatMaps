201bdc457e768f8c76910e0912f4073f6c5efd12
AngelAzrail <androsovdima99@gmail.com>
2022-08-03 22:41:24 +0300

    fix redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.scss';
 import { Map } from 'ol';
+import { Provider } from 'react-redux';
 import MapContainer from './ui/Map/Map';
+import store from './store/store';
 
 const App = (): JSX.Element => {
   const map = new Map({ controls: [] });
-  return <MapContainer map={map} />;
+  return (
+    <Provider store={store}>
+      <MapContainer map={map} />
+    </Provider>
+  );
 };
 
 export default App;
